9091f1423edf979a28f46af6a7ce83413563b9bb
Mars11 <1126520683@qq.com>
2021-12-23 15:06:39 +0800

    change inner class to public

---
+++ algo/src/main/java/cn/cqsztech/initial/InitialTest.java
@@ -0,0 +1,16 @@
+package cn.cqsztech.initial;
+
+/**
+ * ccmars
+ * 2021/12/23
+ **/
+public class InitialTest {
+    static int  a;
+    public static void main(String[] args) {
+        System.out.println(a);
+        //局部变量必须初始化
+        int b ;
+        //不初始化会报错
+//        System.out.println(b);
+    }
+}
