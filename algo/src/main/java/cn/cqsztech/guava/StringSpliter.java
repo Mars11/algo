09fce77ea1db7ccf318f0614e8bf84f12d003333
Mars11 <1126520683@qq.com>
2021-12-29 11:27:41 +0800

    guava joiner

---
+++ algo/src/main/java/cn/cqsztech/guava/StringSpliter.java
@@ -0,0 +1,25 @@
+package cn.cqsztech.guava;
+
+import com.google.common.base.Joiner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ccmars
+ * 2021/12/29
+ **/
+public class StringSpliter {
+    public static final  Joiner.MapJoiner MAP_JOINER = Joiner.on("&").withKeyValueSeparator("=");
+    public static String getStrFromMap(Map map){
+        return MAP_JOINER.join(map);
+    }
+
+    public static void main(String[] args) {
+        Map params = new HashMap();
+        params.put("name","zhangsan");
+        params.put("age",12);
+        // assert name=zhangsan&age=12
+        System.out.println(getStrFromMap(params));
+    }
+}
